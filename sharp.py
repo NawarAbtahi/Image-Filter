c7a32fceb1fc805bfe9411264189412dcd5d52f7
Abtahi <nawarabtahi@gmail.com>
2026-07-02 15:40:04 +0600

    Fixed sharp.py and removed duplicating code from main.py

--- sharp.py
+++ sharp.py
@@ -9,10 +9,10 @@ kernel = np.array([
 
 
 def sharp(img_arr, padded_img):
-    blurred_arr = np.array(np.zeros(img_arr.shape))
+    sharpped_arr = np.array(np.zeros(img_arr.shape))
     for row in range(img_arr.shape[0]):
         for column in range(img_arr.shape[1]):
             pixel_matrix = padded_img[row: row+3, column: column+3]
             convolve_img = np.sum(pixel_matrix * kernel)
-            blurred_arr[row, column] = convolve_img
-    return blurred_arr
+            sharpped_arr[row, column] = convolve_img
+    return sharpped_arr
